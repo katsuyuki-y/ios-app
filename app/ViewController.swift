8cc0b8bb291c57727d8ef7da4b52311b4832067e
katsuyuki <github@tsuyki.com>
2021-11-02 03:35:07 +0900

    add webview & tabbar

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webview: WKWebView!
+    let url = "https://google.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let request = URLRequest(url: URL(string: url)!)
+        webview.load(request)
     }
-
-
 }
-
